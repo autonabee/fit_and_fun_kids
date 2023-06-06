2e0a84cb6b75adc902aea835db497cb018cc2c45
Roger Pissard-Gibollet <roger.pissard@inria.fr>
2023-06-06 23:02:55 +0200

    simplify install

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Patch the possibility to communicate with mqtt fit_and_fun sensor
-cp patch/runtime_sensor.py sb3topy/src/engine/
-cp patch/__init__.py sb3topy/src/engine/
-# Python module required
-pip install -r requirements.txt
